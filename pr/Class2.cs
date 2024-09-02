e26b6f329b2a78e2482e76fa5ce9216158a017c0
gitHaq2531 <afzaulhaque1@gmail.com>
2024-09-02 14:31:17 +0530

    class2

---
+++ pr/Class2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoProd.pr
+{
+    internal class Class2
+    {
+        public void Upt()
+        {
+            Console.WriteLine("hye");
+        }
+    }
+}
