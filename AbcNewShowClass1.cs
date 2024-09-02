8ac36ec67b917b8997c5dc47a545b15dcb097bb4
gitHaq2531 <afzaulhaque1@gmail.com>
2024-09-02 14:02:03 +0530

    abc

---
+++ AbcNewShowClass1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shoProd
+{
+    internal class AbcNewShowClass1
+    {
+        public void NewTest ()
+            {
+            Console.WriteLine("heyyy");
+            }
+    }
+}
